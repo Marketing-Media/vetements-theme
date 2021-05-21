6b68cf79cdc2aa912d7eb6a249ae1877cdac1e76
Christian Isai <cill@marketingmedia.ca>
2021-05-21 12:43:05 -0500

    Experimenting with theme mobile settings

---
+++ assets/js/6.js
@@ -0,0 +1,18 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[6],{
+
+/***/ "./css/dynamic/dropdown/_index.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(true) {
+      // 1621618969217
+      var cssReload = __webpack_require__("./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+}]);
+//# sourceMappingURL=6.js.map
