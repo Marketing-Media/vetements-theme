102cee625f39fbb52efb5dc86fa50bb9e11b5024
Christian Isai <cill@marketingmedia.ca>
2021-05-23 23:29:15 -0500

    sass update

---
+++ assets/theme.fa10bd651391a033df6b.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate("theme",{
+
+/***/ "./css/theme.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(true) {
+      // 1621830550637
+      var cssReload = __webpack_require__("./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=theme.fa10bd651391a033df6b.hot-update.js.map
