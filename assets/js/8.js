7cb3366382fa5821e9e878ccc154e1e48982f053
Christian Isai <cill@marketingmedia.ca>
2021-05-23 23:50:33 -0500

    sass stest

--- assets/js/8.js
+++ assets/js/8.js
@@ -5,7 +5,7 @@
 
 // extracted by mini-css-extract-plugin
     if(true) {
-      // 1621830734247
+      // 1621831770546
       var cssReload = __webpack_require__("./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
